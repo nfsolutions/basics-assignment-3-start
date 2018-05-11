84774fb595e072b9ec0b0d1f50d9b0033ec1aa91
nydi <stefan.nydegger@gmail.com>
2018-05-11 12:29:42 +0200

    assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  count = 0;
+  logItems: string[] = [];
+
+  displayDetails() {
+    this.count++;
+    this.logItems.push('Button pressed at time ' + moment(new Date()).format('DD.MM.YYYY h:MM:ss'));
+  }
+
+  showButton(): boolean {
+    return this.count % 2 !== 0;
+  }
+
+  public getColor(i) {
+    return i > 4 ? 'blue' : 'green';
+  }
 }
